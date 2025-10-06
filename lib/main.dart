7d36ca217665f18148d4b63bbbfca8b8c9edc8d7
Chaylane Franco <chayfranco22@gmail.com>
2025-10-05 21:49:22 -0300

    Corrigido a exposição da API KEY

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
-import 'who_is_using.dart'; 
+import 'package:flutter_dotenv/flutter_dotenv.dart'; 
+import 'package:firebase_core/firebase_core.dart';
 
+import 'who_is_using.dart';
 import 'login_page.dart';
 import 'home_page.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp( 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+  
+  // Carregar variáveis do .env
+  await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY']!,
+      authDomain: dotenv.env['AUTH_DOMAIN'],
+      projectId: dotenv.env['PROJECT_ID']!,
+      storageBucket: dotenv.env['STORAGE_BUCKET'],
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['APP_ID']!,
+      measurementId: dotenv.env['MEASUREMENT_ID'],),);
+
   runApp(const MobusApp());
 }
 
